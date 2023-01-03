08d1c2434b092dd285a64324b9346bfadc6fe12c
robosulthan <86226497+robosulthan@users.noreply.github.com>
2023-01-03 14:10:01 +0530

    Update upd.sh

--- upd.sh
+++ upd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 IP1=`curl -s http://checkip.amazonaws.com`
 echo "Updatin $IP1 in settings file"
-sed -i 's/ipxxxx/'"$IP1"'/' ikSaan/settings.py
+sed -i 's/ipxxxx/'"$IP1"'/' ikSite/settings.py
